6681bce434c6fdb90b772a520e2a97445bc2eefd
Yi Jia <yi@investmentmarkets.com.au>
2024-04-02 16:21:49 +1000

    add login

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,24 @@
 'use client';
 
 import { useChat } from 'ai/react';
+import { useEffect, useState } from 'react';
 
 export default function Chat() {
+  const [loggedIn, setLoggedIn] = useState(false)
+  const [passwd, setPasswd] = useState('')
   const { messages, input, handleInputChange, handleSubmit } = useChat();
+
+  const handlePasswdChange = (e) => {
+    setPasswd(e.target.value)
+  }
+
+  const handleLogIn = (e) => {
+    e.preventDefault()
+    if (passwd === process.env.NEXT_PUBLIC_FAKE_USER_PASSWORD) {
+      setLoggedIn(true)
+    }
+  }
+
   return (
     <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
       {messages.map(m => (
@@ -13,14 +28,25 @@ export default function Chat() {
         </div>
       ))}
 
-      <form onSubmit={handleSubmit}>
+      {loggedIn ?
+        <form onSubmit={handleSubmit}>
+          <input
+            className="fixed bottom-0 w-full max-w-md p-2 mb-8 border border-gray-300 rounded shadow-xl"
+            value={input}
+            placeholder="Say something..."
+            onChange={handleInputChange}
+          />
+        </form> :
+        <form onSubmit={handleLogIn}>
         <input
+          type="password"
           className="fixed bottom-0 w-full max-w-md p-2 mb-8 border border-gray-300 rounded shadow-xl"
-          value={input}
-          placeholder="Say something..."
-          onChange={handleInputChange}
+          value={passwd}
+          placeholder="Please input the password"
+          onChange={handlePasswdChange}
         />
       </form>
+      }
     </div>
   );
 }
