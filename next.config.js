e5ab03fb329255cd0f69dcd7ae4509de83bb3ca0
charjia999 <165757985+charjia999@users.noreply.github.com>
2024-04-18 16:00:19 +1000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: "/my-chatbot",
-  output: "export",
-  experimental: {
-    serverActions: false,
-  },
 };
 
 module.exports = nextConfig;
